2d2e8d51d87dad0419f6b110bf2df511a7cb10d6
JayanthReddy3010 <gjayanthreddy69@gmail.com>
2026-05-24 13:07:09 +0530

    fixed postgres production connection

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "studygroupfinder",
-  password: "root",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
