23760ad7bdfd03122a4688a4d338667eacf7db39
JayanthReddy3010 <gjayanthreddy69@gmail.com>
2026-05-24 13:45:31 +0530

    fixed cors

--- server.js
+++ server.js
@@ -17,13 +17,14 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://study-group-frontend-f68ua6heq-gjayanthreddy69-2878s-projects.vercel.app"
+      "http://localhost:3000",
+      "https://study-group-frontend-3tvwhkvct-gjayanthreddy69-2878s-projects.vercel.app"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
   })
 );
-
+app.options("*", cors());
 /* =========================
    ROUTES
 ========================= */
